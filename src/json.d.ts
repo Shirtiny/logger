b37ace29aa27d302f337a9c8a5a39edd381fe917
shirtiny <shirtiny@gmail.com>
2021-09-15 16:14:30 +0800

    修改默认导出的模块名

--- src/json.d.ts
+++ src/json.d.ts
@@ -4,10 +4,5 @@
  * @LastEditTime: 2021-06-25 11:46:23
  * @Description:
  */
-/*
- * @Author: Shirtiny
- * @Date: 2021-06-25 11:43:20
- * @LastEditTime: 2021-06-25 11:43:21
- * @Description:
- */
+
 declare module "*.json";
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: Shirtiny
  * @Date: 2021-06-11 10:01:14
- * @LastEditTime: 2021-06-25 16:30:10
+ * @LastEditTime: 2021-09-15 16:13:52
  * @Description:
  */
 import { LEVELS, ShLogger } from "./model/shLogger";
@@ -11,8 +11,8 @@ import { Theme } from "./style/theme";
 
 const theme = new Theme();
 
-const logger = new ShLogger();
+const sh_log = new ShLogger();
 
 export { theme, ShLogger, css, Logger, LEVELS };
 
-export default logger;
+export default sh_log;
